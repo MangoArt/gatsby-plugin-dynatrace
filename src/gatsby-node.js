f01b363e8b978530ef21237847d51a53bfe59af0
Markus Oehler <m.oehler@digital-city.solutions>
2019-04-21 17:41:34 +0200

    Added missing files

---
+++ src/gatsby-node.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import * as fs from 'fs';
+
+exports.onPreBootstrap = function(helpers, pluginOptions) {
+
+  const modes = ['jsTag', 'asyncCS', 'syncCS', 'jsInlineScript'];
+  const {
+    tenantId,
+    applicationId,
+    apiToken,
+    mode
+  } = pluginOptions;
+
+  const {
+    reporter
+  } = helpers;
+
+  const urlToJSAgentSnippet = `https://${tenantId}.live.dynatrace.com/api/v1/rum/${modes[mode]}/${applicationId}?Api-Token=${apiToken}`;
+
+  return fetch(urlToJSAgentSnippet)
+    .then((response) => {
+      // unzip and return text
+      return response.text();
+    })
+    .then((response) => {
+      fs.writeFileSync("./.cache/dynatrace.js", response, { encoding: 'utf8'});
+      reporter.info('Successfully retrieved dynatrace JS Agent snippet!');
+    })
+    .catch((error) => {
+      reporter.error('Failed to retrieve dynatrace JS Agent snippet!', error);
+    });
+}
---
+++ src/gatsby-ssr.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import * as fs from 'fs';
+import HTMLReactParser from 'html-react-parser';
+
+exports.onRenderBody = function(apiCallbackContext, pluginOptions) {
+
+  if (process.env.NODE_ENV !== `production` && process.env.NODE_ENV !== `test`) {
+    return null
+  }
+
+  const { mode, enabled } = pluginOptions;
+  var script = fs.readFileSync("./.cache/dynatrace.js", {encoding: 'utf-8'});
+  var parsedScript = HTMLReactParser(script);
+
+  if (!parsedScript || !enabled) {
+    return false;
+  }
+
+  const {
+    setHeadComponents,
+  } = apiCallbackContext;
+
+  const setComponents = setHeadComponents;
+
+  if (mode === 0) {
+    return setComponents([
+      <script
+        key="gatsby-plugin-dynatrace"
+        src={parsedScript.props.src}
+        data-dtconfig={parsedScript.props['data-dtconfig']}
+        crossorigin="anonymous"
+      />,
+    ]);
+  } else if (mode === 1 || mode === 2) {
+    return setComponents([
+      <script
+        key="gatsby-plugin-dynatrace"
+        type="text/javascript"
+        dangerouslySetInnerHTML={parsedScript.props.dangerouslySetInnerHTML}
+      />,
+    ]);
+  } else if (mode === 3) {
+    return setComponents([
+      <script
+        key="gatsby-plugin-dynatrace"
+        type="text/javascript"
+        data-dtconfig={parsedScript.props['data-dtconfig']}
+        dangerouslySetInnerHTML={parsedScript.props.dangerouslySetInnerHTML}
+      />,
+    ]);
+  }
+}
